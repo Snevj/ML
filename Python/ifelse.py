0cd1b120aec64af44d70e9b7ef27059786b28fdf
Snevj <krishanagopal236606@gmail.com>
2025-06-04 00:19:01 +0530

    If-else completed

--- Python/ifelse.py
+++ Python/ifelse.py
@@ -4,12 +4,12 @@ time = int(input("Enter the time Sir"))
 #you have to enter the time wrt the 24 hrs system
 
 if(12>time>0):
-    print("Good Moring Sir")
+    print("Good Morning Sir")
 elif(time == 12):
     print("Good Noon Sir")
 elif(12>time>17):
     print("Good Afternoon Sir!")
 elif(24>time>17):
-        print("Good Night Sir!")
+    print("Good Night Sir!")
 else:
-     print("Please enter the time according to 24 hrs format")
+    print("Please enter the time according to 24 hrs format")
