6f30b3f0c2b53d8277b096b6fa0aacb5c6921c7b
Snevj <krishanagopal236606@gmail.com>
2025-06-04 17:08:33 +0530

    while loops with else

--- Python/loops.py
+++ Python/loops.py
@@ -1,3 +1,11 @@
 #doing the for loops
 for i in range(1, 12, 2):
     print(i)
+
+#doing the while loops
+count = 5
+while (count>=0):
+    print(count)
+    count = count - 1
+else:
+    print("we are outside the loop")
