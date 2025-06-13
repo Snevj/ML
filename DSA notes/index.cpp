3f352ebc58e470b579732d71dd82a81a65999fa2
Snevj <krishanagopal236606@gmail.com>
2025-06-13 11:50:32 +0530

    practising dsa on cpp

---
+++ DSA notes/index.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int Sum(int n){
+    if (n == 0){
+        return 0;
+    }
+    
+    int PrevSum = Sum(n-1);
+    return n + PrevSum;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<Sum(n)<<endl;
+    return 0;
+}
